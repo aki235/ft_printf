f07fe0d41a08ecaa9d01acaecb3ea429aed51711
aktomiza <aktomiza@student.42tokyo.jp>
2023-08-29 14:46:42 +0900

    modifiedfrom42

--- ft_printf.c
+++ ft_printf.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aktomiza <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/29 14:45:16 by aktomiza          #+#    #+#             */
+/*   Updated: 2023/08/29 14:45:19 by aktomiza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-int	printf_conversions(char c)
+int	check_format(char c)
 {
-	
+
 }
 
 int	ft_printf(const char *format, ...)
@@ -12,6 +24,10 @@ int	ft_printf(const char *format, ...)
 	va_start(args, format);
 	while (*format != '\0')
 	{
-		
+		if (*format == '%')
+		{
+			if (!*(++format))
+				
+		}
 	}
 }
